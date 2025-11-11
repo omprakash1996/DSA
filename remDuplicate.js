9178161eeb87dc7a444567f0b372eeb06733aeac
OMPRAKASH BEHERA <omprakash>
2025-11-11 21:55:06 +0530

    remove Duplicate

---
+++ remDuplicate.js
@@ -0,0 +1,19 @@
+// Remove duplicates from an array.
+function removeDuplicates(arr){
+    let result=[];
+    for(let i=0;i<arr.length;i++){
+        let isDuplicate=false;
+        for(let j=0;j<result.length;j++){
+            if(arr[i]===result[j]){
+                isDuplicate=true;
+                break;
+            }
+        }
+        if (!isDuplicate){
+            result[result.length]=arr[i];
+        }
+    }
+    return result;
+}
+let input=[1,2,3,4,3,5,7,2,1];
+console.log(removeDuplicates(input));
